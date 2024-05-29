8db55614cbfc56dc93d8ff6158cbdc1c3003a72a
saramaria06 <sara.cerquera@genstudents.org>
2024-05-29 10:04:52 -0500

    funcion javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function convertirTemperatura() {
+    let input = prompt("Ingrese la temperatura en grados Celsius:");
+    let celsius = parseFloat(input);
+
+    // Verificar si la entrada es un número
+    if (isNaN(celsius)) {
+        console.error("Error: La entrada no es un número.");
+        convertirTemperatura(); // Volver a solicitar los datos
+        return;
+    }
+
+    let fahrenheit = celsius * 9 / 5 + 32;
+    let kelvin = celsius + 273.15;
+
+    console.log("Grados Fahrenheit:", fahrenheit.toFixed(2));
+    console.log("Grados Kelvin:", kelvin.toFixed(2));
+}
+
+convertirTemperatura();
